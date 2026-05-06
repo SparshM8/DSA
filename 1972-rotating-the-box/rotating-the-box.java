2e05fa1b292d020469080cfbbe3f323ad2d702f5
Sparsh Mishra <callme8samay@gmail.com>
2026-05-06 23:06:40 +0530

    Time: 6 ms (99.52%) | Memory: 125.2 MB (5.53%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -1,31 +1,23 @@
-
 class Solution {
     public char[][] rotateTheBox(char[][] boxGrid) {
-        int m = boxGrid.length;       
-        int n = boxGrid[0].length;     
-        char[][] result = new char[n][m];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                result[j][m - 1 - i] = boxGrid[i][j];
+        int m=boxGrid.length;
+        int n=boxGrid[0].length;           
+        char[][]ch=new char[n][m];
+        for(char[]row:ch){
+            Arrays.fill(row,'.');
+        }
+        for(int i=0;i<m;i++){
+            int empty=n-1;
+            for(int j=n-1;j>=0;j--){
+               if(boxGrid[i][j]=='*'){
+                ch[j][m-1-i]='*';
+                empty=j-1;
+               }else if(boxGrid[i][j]=='#'){
+                ch[empty][m - 1 - i] = '#';
+                 empty--;
+               }
             }
         }
-        int[] emptyIndex = new int[m];           
-        Arrays.fill(emptyIndex, n - 1);
-        for (int i = n - 1; i >= 0; i--) {      
-            for (int j = 0; j < m; j++) {        
-                if (result[i][j] == '#') {
-                    if (emptyIndex[j] != i) {
-                        result[emptyIndex[j]][j] = '#'; 
-                        result[i][j] = '.';             
-                    }
-                    emptyIndex[j]--;
-
-                } else if (result[i][j] == '*') {
-                    emptyIndex[j] = i - 1;
-                }
-            }
-        }
-        return result;
+        return ch;
     }
 }
