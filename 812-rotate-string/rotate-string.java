2ef05282744f8fa256be9aa05d9f1edc10597c00
Sparsh Mishra <callme8samay@gmail.com>
2026-06-28 02:24:41 +0530

    Time: 2 ms (12.90%) | Memory: 43.7 MB (5.67%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -3,14 +3,7 @@ class Solution {
         if (s.length() != goal.length()) {
             return false;
         }
-
-        String word = s + s;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (word.substring(i, i + s.length()).equals(goal)) {
-                return true;
-            }
-        }
-        return false;
+        String doubledString = s + s;
+        return doubledString.contains(goal);
     }
 }
