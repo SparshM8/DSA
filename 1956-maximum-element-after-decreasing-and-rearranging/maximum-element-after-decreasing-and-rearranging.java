c8ee97a003b885896624fd527ee32ccebafa8170
Sparsh Mishra <callme8samay@gmail.com>
2026-06-28 12:45:18 +0530

    Time: 9 ms (67.50%) | Memory: 77.5 MB (61.67%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
@@ -1,15 +1,26 @@
+// class Solution {
+//     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+//         int n = arr.length;
+//         Arrays.sort(arr);
+//         if (arr[0] != 1) {
+//             arr[0] = 1;
+//         }
+//         for (int i = 1; i < n; i++) {
+//             if (arr[i] - arr[i - 1] > 1) {
+//                 arr[i] = arr[i - 1] + 1;
+//             }
+//         }
+//         return arr[n - 1];
+//     }
+// }
+
 class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-        int n = arr.length;
         Arrays.sort(arr);
-        if (arr[0] != 1) {
-            arr[0] = 1;
+        int maxElement = 0;
+        for (int num : arr) {
+            maxElement = Math.min(maxElement + 1, num);
         }
-        for (int i = 1; i < n; i++) {
-            if (arr[i] - arr[i - 1] > 1) {
-                arr[i] = arr[i - 1] + 1;
-            }
-        }
-        return arr[n - 1];
+        return maxElement;
     }
 }
