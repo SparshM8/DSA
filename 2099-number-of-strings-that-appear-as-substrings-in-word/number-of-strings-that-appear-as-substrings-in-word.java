e1527223733415908d60d76669bf9c13e627f6c9
Sparsh Mishra <callme8samay@gmail.com>
2026-06-30 00:43:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (86.75%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -10,14 +10,43 @@
 //     }
 // }
 
+// class Solution {
+//     public int numOfStrings(String[] patterns, String word) {
+//         int ans = 0;
+//         for (String str : patterns) {
+//             if (word.indexOf(str) != -1) {
+//                 ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+// class Solution {
+//     public int numOfStrings(String[] patterns, String word) {
+//         int ans = 0;
+//         for (String str : patterns) {
+//             // .* means "any characters before or after"
+//             if (word.matches(".*" + Pattern.quote(str) + ".*")) {
+//                 ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int numOfStrings(String[] patterns, String word) {
-        int ans = 0;
-        for (String str : patterns) {
-            if (word.indexOf(str) != -1) {
-                ans++;
+        int count = 0;
+        
+        // Loop through each pattern
+        for (String pattern : patterns) {
+            // Read it out loud: "If the word contains the pattern, count it."
+            if (word.contains(pattern)) {
+                count++;
             }
         }
-        return ans;
+        
+        return count;
     }
 }
