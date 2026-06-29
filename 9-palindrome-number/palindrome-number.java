5346db5425b3443bcdbfda061efca76399fb8910
Sparsh Mishra <callme8samay@gmail.com>
2026-06-30 01:27:15 +0530

    Time: 8 ms (8.93%) | Memory: 46.3 MB (6.74%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,16 +1,25 @@
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         if(x<0){
+//             return false;
+//         }
+//         int rev = 0;
+//         int  num= x;
+
+//         while (num!= 0) {
+//             rev= rev*10 + num%10;
+//             num=num/10;
+//         }
+
+//         return (rev == x);
+//     }
+// }
+
+
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0){
-            return false;
-        }
-        int rev = 0;
-        int  num= x;
-
-        while (num!= 0) {
-            rev= rev*10 + num%10;
-            num=num/10;
-        }
-
-        return (rev == x);
+        String original = String.valueOf(x);
+        String reversed = new StringBuilder(original).reverse().toString();      
+        return original.equals(reversed);
     }
 }
