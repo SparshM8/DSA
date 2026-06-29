0c922d7cc7b390ae0ef549a72005687143b2f80c
Sparsh Mishra <callme8samay@gmail.com>
2026-06-30 00:40:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (94.07%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -1,5 +1,17 @@
+// class Solution {
+//     public int numOfStrings(String[] patterns, String word){
+//         int ans = 0;
+//         for (String str : patterns) {
+//             if (word.indexOf(str) != -1) {
+//                 ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
-    public int numOfStrings(String[] patterns, String word){
+    public int numOfStrings(String[] patterns, String word) {
         int ans = 0;
         for (String str : patterns) {
             if (word.indexOf(str) != -1) {
