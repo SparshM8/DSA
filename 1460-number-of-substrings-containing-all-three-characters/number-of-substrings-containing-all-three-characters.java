18380ea612cb0cc22e865812cc23f6c9095f4d42
Sparsh Mishra <callme8samay@gmail.com>
2026-07-01 03:51:59 +0530

    Time: 13 ms (72.58%) | Memory: 46.4 MB (30.64%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -16,17 +16,35 @@
 //     }
 // }
 
+// class Solution {
+//     public int numberOfSubstrings(String s) {
+//         int[] lastSeen = {-1, -1, -1};
+//         int ans = 0;
+        
+//         for (int i = 0; i < s.length(); i++) {
+//             lastSeen[s.charAt(i) - 'a'] = i;
+            
+//             int minIndex = Math.min(lastSeen[0], Math.min(lastSeen[1], lastSeen[2]));
+            
+//             ans += (minIndex + 1);
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int numberOfSubstrings(String s) {
-        int[] lastSeen = {-1, -1, -1};
         int ans = 0;
+        int[] last = {-1, -1, -1}; 
         
         for (int i = 0; i < s.length(); i++) {
-            lastSeen[s.charAt(i) - 'a'] = i;
+            last[s.charAt(i) - 'a'] = i;
             
-            int minIndex = Math.min(lastSeen[0], Math.min(lastSeen[1], lastSeen[2]));
+            int minIndex = Math.min(last[0], Math.min(last[1], last[2]));
             
-            ans += (minIndex + 1);
+            if (minIndex != -1) {
+                ans += (minIndex + 1);
+            }
         }
         return ans;
     }
