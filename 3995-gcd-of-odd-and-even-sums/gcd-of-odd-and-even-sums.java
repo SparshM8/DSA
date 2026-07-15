cdd790616b7694f112fba08926cedb5f331bf529
Sparsh Mishra <callme8samay@gmail.com>
2026-07-15 21:51:08 +0530

    Time: 1 ms (83.11%) | Memory: 42.8 MB (6.12%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,15 +1,23 @@
+// class Solution {
+//     public int gcdOfOddEvenSums(int n) {
+//         int SumO = n*n;
+//         int SumE = n*(n+1);
+//         return gcd(SumO, SumE);
+//     }
+//     private int gcd(int a, int b){
+//         while(b!=0){
+//             int temp =b;
+//             b = a%b;
+//             a =temp;
+//         }
+//         return a;
+//     }
+// }
+
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int SumO = n*n;
-        int SumE = n*(n+1);
-        return gcd(SumO, SumE);
-    }
-    private int gcd(int a, int b){
-        while(b!=0){
-            int temp =b;
-            b = a%b;
-            a =temp;
-        }
-        return a;
+        int odd = n*n;
+        int even = n*(n+1);
+        return n;
     }
 }
