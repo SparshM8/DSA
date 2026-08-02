5d12b88b3255b86ff7f8f1a29663c13164961ee8
Sparsh Mishra <callme8samay@gmail.com>
2026-08-02 09:35:45 +0530

    Time: 5 ms (23.31%) | Memory: 43.3 MB (35.84%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,5 +1,15 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        int[] dp = piles.clone();
+        
+        for (int length = 2; length <= n; length++) {
+            for (int i = 0; i <= n - length; i++) {
+                int j = i + length - 1;
+                dp[i] = Math.max(piles[i] - dp[i+1], piles[j] - dp[i]);
+            }
+        }
+        
+        return dp[0] > 0;
     }
 }
