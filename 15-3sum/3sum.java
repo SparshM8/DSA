57a326f4826ba444d40dfc2aaed5aa3a9ac75061
Sparsh Mishra <callme8samay@gmail.com>
2026-08-04 22:44:34 +0530

    Time: 33 ms (73.33%) | Memory: 59.1 MB (60.34%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,30 +1,34 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) { 
-        List<List<Integer>> arr = new ArrayList<>();
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        for (int i=0; i<nums.length; i++){
-            if (i>0 && nums[i] == nums[i-1]){
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int j = i+1;
-            int k = nums.length - 1;
-            while (j<k){
-                int s = nums[i]+ nums[j]+ nums[k];
-                if (s > 0){
-                    k -= 1;
-                }
-                else if (s < 0){
-                    j += 1;
-                }
-                else{
-                    arr.add(new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[k]))); 
-                    j+=1;
-                    while (j<k && nums[j] == nums[j-1]){
-                        j+=1;
-                    }
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                  
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
-        }
-        return arr;
+        }       
+        return result;
     }
 }
