05d7616d5ab131e3c636e62759938badedc2975a
Sparsh Mishra <callme8samay@gmail.com>
2026-08-31 19:35:41 +0530

    Time: 9 ms (23.01%) | Memory: 108.3 MB (7.46%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -10,46 +10,45 @@
  */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-      ListNode prev=null, curr=head;
-      ArrayList<Integer> points=new ArrayList<>();
-      int[] res={-1,-1};
-      int idx=1;
 
-      while(curr.next!=null)
-      {
-        if(prev==null)
-        {
-          prev=curr;
-          curr=curr.next;
-          idx++;
-          continue;
+        int[] res = {-1, -1};
+
+        ListNode prev = head;
+        ListNode curr = head.next;
+
+        int idx = 2;
+
+        ArrayList<Integer> points = new ArrayList<>();
+
+        while (curr.next != null) {
+
+            // Local maxima
+            if (curr.val > prev.val && curr.val > curr.next.val) {
+                points.add(idx);
+            }
+
+            // Local minima
+            if (curr.val < prev.val && curr.val < curr.next.val) {
+                points.add(idx);
+            }
+
+            prev = curr;
+            curr = curr.next;
+            idx++;
         }
-        // condition for Local maxima
-        if(prev.val<curr.val && curr.val>curr.next.val)
-          points.add(idx);
-        
-        // condition for Local minima
-        if(prev.val>curr.val && curr.val<curr.next.val)
-          points.add(idx);
 
-         prev=curr;
-         curr=curr.next;
-         idx++;
-      }
+        if (points.size() < 2) {
+            return res;
+        }
 
-       if(points.size()>=2)
-       {
-         int min=Integer.MAX_VALUE;
-         for(int i=1;i<points.size();i++)
-         {
-            int dist=points.get(i)-points.get(i-1);
-            if(dist<min)
-              min=dist;
-         }
-         res[0]=min;
-         res[1]=points.get(points.size()-1)-points.get(0);
-       }
+        int min = Integer.MAX_VALUE;
 
-       return res; 
+        for (int i = 1; i < points.size(); i++) {
+            min = Math.min(min, points.get(i) - points.get(i - 1));
+        }
+
+        int max = points.get(points.size() - 1) - points.get(0);
+
+        return new int[]{min, max};
     }
 }
