5e8e680e2497023220dcd31db526490a1137cef9
Sparsh Mishra <callme8samay@gmail.com>
2026-09-11 23:27:08 +0530

    Time: 5 ms (82.80%) | Memory: 46.4 MB (46.93%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -1,29 +1,22 @@
 class Solution {
     public int totalNumbers(int[] digits) {
-        int[] freq = new int[10];
-        for (int d : digits) {
-            freq[d]++;
-        }
-        
-        int validCount = 0;
-        
-        for (int num = 100; num <= 998; num += 2) {
-            int d1 = num / 100;          
-            int d2 = (num / 10) % 10;    
-            int d3 = num % 10;           
-            
-            int[] needed = new int[10];
-            needed[d1]++;
-            needed[d2]++;
-            needed[d3]++;
-            
-            if (freq[d1] >= needed[d1] && 
-                freq[d2] >= needed[d2] && 
-                freq[d3] >= needed[d3]) {
-                validCount++;
+
+        Set<Integer>set=new HashSet<>();
+        for(int i=0;i<digits.length;i++){
+            for(int j=0;j<digits.length;j++){
+                for(int k=0;k<digits.length;k++){
+                    if(i==j||j==k||i==k)
+                    continue;
+                    if(digits[i]==0)
+                    continue;
+                    if(digits[k]%2!=0)
+                    continue;
+                    int num=digits[i]*100+digits[j]*10+digits[k];
+                    set.add(num);
+
+                }
             }
         }
-        
-        return validCount;
+        return set.size();
     }
 }
